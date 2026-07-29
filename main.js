0ca19ddac06f024014bc02d3b2a402943b1ad44f
sljcdrlvecc <Ssfikri222@hotmail.com>
2026-07-30 00:12:30 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+/* TOGGLE SHARE */
+const shareBtn = document.getElementById("shareBtn");
+const shareLinks = document.getElementById("shareLinks");
+
+shareBtn.onclick = () => {
+    window.open("https://www.facebook.com/share/g/1HAGTStoyr/", "_blank")
+};
+/* UNDANGAN */
+function joinWhatsAppGroup() {
+    window.open("https://www.facebook.com/share/g/1HAGTStoyr/");
+}
+
+function openFacebookPage() {
+    window.open("https://facebook.com/groups/1407008471329470/");
+}
+const video = document.getElementById("video");
+const overlay = document.getElementById("videoOverlay");
+
+let overlayClicked = false; 
+
+// Overlay muncul di detik tertentu
+video.addEventListener("timeupdate", () => {
+    if (video.currentTime >= 1 && !overlayClicked) {
+        overlay.classList.add("show");
+    }
+});
+
+// Klik overlay
+overlay.addEventListener("click", () => {
+    overlayClicked = true;              
+    overlay.style.display = "none";   
+    overlay.classList.remove("show");
+
+    // Aksi setelah klik
+    window.open("https://s.shopee.co.id/8zyp8hZrSG", "_blank");
+});
+
+
+
+
